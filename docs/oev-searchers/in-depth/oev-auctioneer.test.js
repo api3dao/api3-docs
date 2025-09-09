581d78715bec5f08a767a1d840c1ba71bdb4d768
Markus Coetzee <mks.coetzee@gmail.com>
2025-09-09 12:28:49 +0200

    Convert test to JS and remove typescript

--- docs/oev-searchers/in-depth/oev-auctioneer.test.js
+++ docs/oev-searchers/in-depth/oev-auctioneer.test.js
@@ -6,23 +6,18 @@ import { getChains } from '@api3/dapi-management';
 
 const supportedMainnets = getChains()
   .filter((chain) => chain.stage === 'active')
-  .map((chain) => CHAINS.find((api3Chain) => api3Chain.id === chain.id)!)
+  .map((chain) => CHAINS.find((api3Chain) => api3Chain.id === chain.id))
   .filter((chain) => !chain.testnet)
   .filter((chain) => chain.alias !== 'oev-network')
   .map((chain) => chain.name)
   .sort((a, b) => a.localeCompare(b));
 
 test('the auction length section lists all the supported chains', () => {
-  const docPath = path.join(
-    process.cwd(),
-    'docs',
-    'oev-searchers',
-    'in-depth',
-    'oev-auctioneer.md'
-  );
-  const content = fs.readFileSync(docPath, 'utf8');
-
-  const chains = extractContentBetweenMarkers(content, 'chain-auction-length-table')
+  const content = readMarkdownFile();
+  const chains = extractContentBetweenMarkers(
+    content,
+    'chain-auction-length-table'
+  )
     .split('\n')
     .filter(Boolean) // Get rid of blank lines
     .slice(2) // Get rid of the header rows
@@ -31,13 +26,29 @@ test('the auction length section lists all the supported chains', () => {
   expect(chains).toStrictEqual(supportedMainnets);
 });
 
-function extractContentBetweenMarkers(content: string, key: string) {
+function readMarkdownFile() {
+  const docPath = path.join(
+    process.cwd(),
+    'docs',
+    'oev-searchers',
+    'in-depth',
+    'oev-auctioneer.md'
+  );
+  if (!fs.existsSync(docPath)) {
+    throw new Error(`Could not find file "oev-auctioneer.md" at "${docPath}"`);
+  }
+  return fs.readFileSync(docPath, 'utf8');
+}
+
+function extractContentBetweenMarkers(content, key) {
   const beginMarker = `<!-- BEGIN:${key} -->`;
   const endMarker = `<!-- END:${key} -->`;
   const beginIndex = content.indexOf(beginMarker);
   const endIndex = content.indexOf(endMarker);
   if (beginIndex === -1 || endIndex === -1) {
-    throw new Error(`Could not find markers "${beginMarker}" and "${endMarker}"`);
+    throw new Error(
+      `Could not find markers "${beginMarker}" and "${endMarker}"`
+    );
   }
   return content.slice(beginIndex + beginMarker.length, endIndex).trim();
 }
